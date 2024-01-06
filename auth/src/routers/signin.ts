6fa36068b18d55b988cc5bec22ef8cf689cfca50
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-01-06 14:21:23 +0300

    Hashing Added

--- auth/src/routers/signin.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.post('api/users/signin', (req, res) => {
-  res.send('Hi there from router');
-});
-
-export { router as signinRouter }; //as using for giving alias
--- auth/src/routers/signout.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.post('api/users/signout', (req, res) => {
-  res.send('Hi there from router');
-});
-
-export { router as signoutRouter }; //as using for giving alias
